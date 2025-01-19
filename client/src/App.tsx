a58c761267f4fffaf00ef24dbad89e24b1baf99f
byt3nomad <byt3nomad>
2025-01-19 19:32:34 +0200

    add chacra ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import { HStack } from "@chakra-ui/react";
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <>App</>;
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
+import { Provider } from "./components/ui/provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
